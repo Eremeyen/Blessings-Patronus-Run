9ec45325fb5544883476bdd70bea69b952deb370
Eremeyen <60618914+Eremeyen@users.noreply.github.com>
2022-05-31 00:38:50 +0300

    "added metadata functionality"

--- ImageCombiner.py
+++ ImageCombiner.py
@@ -1,3 +1,5 @@
+from shutil import which
+from tkinter import W
 from PIL import Image
 import os
 from os import listdir
@@ -15,6 +17,59 @@ hair = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Patronus Run\Hair"
 crown = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Patronus Run\crown"
 horn = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Patronus Run\Horn"
 orbs = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Patronus Run\orbs"
+
+#dictionaries
+background_names = {
+    "PATRONUS BACKGROUND 1.png":"Purple",
+    "PATRONUS BACKGROUND 2.png":"Purple blue",
+    "PATRONUS BACKGROUND 3.png":"Navy",
+    "PATRONUS BACKGROUND 4.png":"Blue in the dark",
+    "PATRONUS BACKGROUND 5.png":"Neon blue in the dark"
+}
+backW_names = {
+    "PATRONUS BACK WING 1.png":"Blue wings",
+    "PATRONUS BACK WING 2.png":"Sky blue wings",
+    "PATRONUS BACK WING 3.png":"Purple wings",
+    "PATRONUS BACK WING 4.png":"Pink sparkle wings",
+    "PATRONUS BACK WING 5.png":"Blue green glow wings",
+    "PATRONUS BACK WING 6.png":"Purple blue glow wings"
+}
+tail_names = {
+    "PATRONUS TAIL 1.png":"White purple glow tail",
+    "PATRONUS TAIL 1.png":"Blue tail",
+    "PATRONUS TAIL 1.png":"Pink blue glow tail"
+}
+body_names = {
+
+}
+
+eye_names = {
+
+}
+
+accessory_names = {
+
+}
+
+fWing_names = {
+
+}
+
+hair_names = {
+
+}
+
+crown_names = {
+
+}
+
+horn_names = {
+
+}
+
+orb_names = {
+
+}
 
 #final images destination
 finalImages = r"C:\Users\meren\OneDrive\Masaüstü\Blessings Patronus Run\Blessings"
@@ -157,34 +212,79 @@ def whichOrbs():
 
 
 
-
+Metadata = {}
 
 while(bCounter < 116):
-    images[0] = Image.open(backgrounds + "/" + whichBackground()).convert("RGBA")
+    blessing = {}
+    backG = whichBackground()
+    images[0] = Image.open(backgrounds + "/" + backG).convert("RGBA")
+    blessing["Background"] = background_names[backG]
+
     backW = whichBackWing()
     images[1] = Image.open(back_wings + "/" + backW).convert("RGBA")
+    blessing["Back Wing"] = backW_names[backW]
+    
     wTail = whichTail()
     images[2] = Image.open(tail + "/" + wTail).convert("RGBA")
-    images[3] = Image.open(bodies + "/" + whichBody()).convert("RGBA")
-    images[4] = Image.open(eyes + "/" + whichEye()).convert("RGBA")
-    images[5] = Image.open(accessories + "/" + whichAccessory()).convert("RGBA")
+    blessing["Tail"] = tail_names[wTail]
+
+    wBody = whichBody()
+    images[3] = Image.open(bodies + "/" + wBody).convert("RGBA")
+    blessing["Body"] = body_names[wBody]
+
+    wEye = whichEye()
+    images[4] = Image.open(eyes + "/" + wEye).convert("RGBA")
+    blessing["Eyes"] = eye_names[wEye]
+
+    wAccessory = whichAccessory()
+    images[5] = Image.open(accessories + "/" + wAccessory).convert("RGBA")
+    blessing["Accessory"] = accessory_names[wAccessory]
+
+
     #determining which front wing to use
     fWing = "PATRONUS FRONT WING " + backW[-5] + ".png"
     images[6] = Image.open(front_wings + "/" + fWing).convert("RGBA") #front wing
+    blessing["Front Wing"] = fWing_names[fWing]
+
     #determining which hair to use
     wHair = "PATRONUS HAIR " + wTail[-5] + ".png"
     images[7] = Image.open(hair + "/" + wHair).convert("RGBA") #hair
-    images[8] = Image.open(crown + "/" + whichCrown()).convert("RGBA")
-    images[9] = Image.open(horn + "/" + whichHorn()).convert("RGBA")
-    images[10] = Image.open(orbs + "/" + whichOrbs()).convert("RGBA")
+    blessing["Hair"] = hair_names[wHair]
+
+    wCrown = whichCrown()
+    images[8] = Image.open(crown + "/" + wCrown).convert("RGBA")
+    blessing["Crown"] = crown_names[wCrown]
+
+    wHorn = whichHorn()
+    images[9] = Image.open(horn + "/" + wHorn).convert("RGBA")
+    blessing["Horn"] = horn_names[wHorn]
+
+    wOrb = whichOrbs()
+    images[10] = Image.open(orbs + "/" + wOrb).convert("RGBA")
+    blessing["Orbs"] = orb_names[wOrb]
+
+
     final_image = images[0]
 
     #combining all layers
     for i in range(1,len(images)):
         final_image = Image.alpha_composite(final_image,images[i])
+    
     #saving the image with the correct name and directory
     print(str(bCounter) + " images generated")
     final_image.save(finalImages+"/Blessings Patronus " +str(bCounter)+".png")
+    
+    if(bCounter < 10):
+        id = "000" + str(bCounter)
+    elif(bCounter < 100):
+        id = "00" + str(bCounter)
+    elif(bCounter < 1000):
+        id = "0" + str(bCounter)
+    else:
+        id = str(bCounter)
+    Metadata[id] = blessing
+
+
     bCounter += 1
 
     
